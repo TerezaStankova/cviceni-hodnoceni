32c87b564a55c0a068a94e08fa55bb7008ca954d
Tereza Bright <tereza.bright@gmail.com>
2024-10-24 19:14:33 +0200

    first part

--- src/components/Star/index.jsx
+++ src/components/Star/index.jsx
@@ -1,14 +1,19 @@
 import { useState } from 'react';
 import './style.css';
 
-export const Star = () => {
+export const Star = ({value, onSelect}) => {
   const [glowing, setGlowing] = useState(false);
+
+  // const handleClick = () => {
+  //   setGlowing(true); 
+  //   onSelect(value);
+  // };
   
   const starClass = glowing
     ? 'rating__star rating__star--on'
     : 'rating__star';
 
   return (
-    <div className={starClass}></div>
+    <div onClick={() => { onSelect(value); setGlowing(true); }} className={starClass}></div>
   );
 };
